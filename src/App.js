b04f215f8d644417794bd456c94c3343c288386e
Jonathan Moyett <someguy590@gmail.com>
2020-08-23 14:19:52 -0500

    Add header, navbar, footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Navbar } from './app/Navbar';
+import { Footer } from './app/Footer';
 
 function App() {
   return (
     <Router>
+      <header>
+        <h1>5e Tools</h1>
+        <Navbar />
+      </header>
       <div className="App">
         <Switch>
         </Switch>
       </div>
+      <Footer />
     </Router>
   );
 }

fde1170719bc1a8d18a73bcc58cbc099cbee80d0
Jonathan Moyett <someguy590@gmail.com>
2020-08-25 04:07:43 -0500

    Add hero creator 3e store slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import heroCreator3eReducer from '../features/hero-creator-3e/heroCreator3eSlice';
 
 export default configureStore({
   reducer: {
+    heroCreator3e: heroCreator3eReducer
   }
 });
--- src/features/hero-creator-3e/heroCreator3eSlice.js
+++ src/features/hero-creator-3e/heroCreator3eSlice.js
@@ -1,8 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const heroCreator3eSlice = createSlice({
+const heroCreator3eSlice = createSlice({
     name: 'heroCreator3e',
     initialState: {
+        plotPoints: 0,
+        abilityPoints: 150,
+        powerLevel: 10,
+        abilitiesCost: 0,
+        powersCost: 0,
+        advantagesCost: 0,
+        skillsCost: 0,
+        defensesCost: 0,
+        heroName: '',
+        playerName: '',
+        identity: '',
+        isIdentitySecret: true,
+        gender: '',
+        age: '',
+        height: '',
+        weight: '',
+        eyes: '',
+        hair: '',
+        groupAffiliation: '',
+        baseOfOperations: '',
+        strength: 0,
+        agility: 0,
+        fighting: 0,
+        awareness: 0,
+        stamina: 0,
+        dexterity: 0,
+        intellect: 0,
+        presence: 0,
+        dodge: 0,
+        parry: 0,
+        fortitude: 0,
+        toughness: 0,
+        will: 0,
     },
     reducers: {
     }

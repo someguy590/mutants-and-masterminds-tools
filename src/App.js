5173201a20150ac3f06704d57854e42364494a44
Jonathan Moyett <someguy590@gmail.com>
2020-08-28 09:25:07 -0500

    Add default route to home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import { Navbar } from './app/Navbar';
 import { Footer } from './app/Footer';
 import { Home } from './features/home/Home';
@@ -17,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/heroCreator3e" component={HeroCreator3e} />
+          <Redirect to="/" />
         </Switch>
       </div>
       <Footer />

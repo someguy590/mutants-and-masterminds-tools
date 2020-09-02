79451fad0efe4f125f7e7f96b7b6f3abe958df9b
Jonathan Moyett <someguy590@gmail.com>
2020-09-02 12:19:55 -0500

    Add strings resources

---
+++ src/resources/strings.js
@@ -0,0 +1,110 @@
+// abilities
+export const strength = 'Strength';
+export const agility = 'Agility';
+export const fighting = 'Fighting';
+export const awareness = 'Awareness';
+export const stamina = 'Stamina';
+export const dexterity = 'Dexterity';
+export const intellect = 'Intellect';
+export const presence = 'Presence';
+
+// defenses
+export const dodge = 'Dodge';
+export const parry = 'Parry';
+export const fortitude = 'Fortitude';
+export const toughness = 'Toughness';
+export const will = 'Will';
+
+// skills
+export const acrobatics = 'Acrobatics';
+export const athletics = 'Athletics';
+export const closeCombat = 'Close Combat';
+export const deception = 'Deception';
+export const expertise = 'Expertise';
+export const insight = 'Insight';
+export const intimidation = 'Intimidation';
+export const investigation = 'Investigation';
+export const perception = 'Perception';
+export const persuation = 'Persuation';
+export const rangedCombat = 'Ranged Combat';
+export const sleightOfHand = 'Sleight Of Hand';
+export const stealth = 'Stealth';
+export const technology = 'Technology';
+export const vehicles = 'Vehicles';
+
+export const abilities = [
+
+];
+
+export const defenses = [
+
+];
+
+export const skills = [
+    acrobatics,
+    athletics,
+    closeCombat,
+    deception,
+    expertise,
+    insight,
+    intimidation,
+    investigation,
+    perception,
+    persuation,
+    rangedCombat,
+    sleightOfHand,
+    stealth,
+    technology,
+    vehicles
+];
+
+export const specifiedSkills = [
+    closeCombat,
+    expertise,
+    rangedCombat,
+];
+
+export const skillHeaders = [
+    'Skills',
+    'Total',
+    'Ability',
+    'Ranks',
+    'Other'
+];
+
+export const heroStats = {
+    abilities: {
+        strength,
+        agility,
+        fighting,
+        awareness,
+        stamina,
+        dexterity,
+        intellect,
+        presence
+    },
+    defenses: {
+        dodge,
+        parry,
+        fortitude,
+        toughness,
+        will
+    },
+    skills: {
+        acrobatics,
+        athletics,
+        closeCombat,
+        deception,
+        expertise,
+        insight,
+        intimidation,
+        investigation,
+        perception,
+        persuation,
+        rangedCombat,
+        sleightOfHand,
+        stealth,
+        technology,
+        vehicles
+    }
+};

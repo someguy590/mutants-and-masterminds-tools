2327368325604761769e608f561c0b81ea7e1b1b
Jonathan Moyett <someguy590@gmail.com>
2020-09-19 07:01:10 -0500

    Add link to ogl

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
     return (
         <footer className="footer">
             <ul>
                 <li>Suggestions? Errors? <a href="mailto:someguy590@gmail.com"><address>Contact Me</address></a></li>
+                <li><Link to="/legal">Legal Information</Link></li>
                 <li>Mutants And Masterminds Tools © 2020</li>
             </ul>
         </footer>

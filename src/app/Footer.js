dc6959c486e664da4a36154e25ceedd48a76eaea
Jonathan Moyett <someguy590@gmail.com>
2020-09-19 07:00:06 -0500

    Add footer

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer>
-            
+        <footer className="footer">
+            <ul>
+                <li>Suggestions? Errors? <a href="mailto:someguy590@gmail.com"><address>Contact Me</address></a></li>
+                <li>Mutants And Masterminds Tools © 2020</li>
+            </ul>
         </footer>
     );
 }
